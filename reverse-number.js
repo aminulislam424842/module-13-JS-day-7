4b90c3594b4586a3889bd1ab17ce7f898d6f328d
Aminul islam <aminulislam424842@gmail.com>
2026-07-27 02:55:45 +0600

    feat: solve reverse number function

--- reverse-number.js
+++ reverse-number.js
@@ -1,5 +1,26 @@
-function reverseNumber (num) {
-  return  [...num].reverse();
+// way-1
+
+
+function reverseNumbers (num) {
+  let str =num.toString();
+  let reverse =str.split("").reverse().join("");
+  return Number(reverse);
 }
-let num =[1,2,5,87,45,63];
-console.log(reverseNumber(num));
+
+console.log(reverseNumbers(1234)); 
+console.log(reverseNumbers(7)); 
+
+
+// way-2
+
+function reverseNum(num) {
+  let reverseStr = num.toString();
+  let reversesNum ="";
+
+  for (let i = reverseStr.length-1; i>=0;i--){
+    reversesNum+=reverseStr[i];
+  }
+  return Number(reversesNum);
+}
+console.log(reverseNum(12345678));
+console.log(reverseNum(7)); 
