5a222c02f7114a444d74effa31c47f0c999f9336
Aminul islam <aminulislam424842@gmail.com>
2026-07-27 11:52:48 +0600

    feat: solve product of digits function

---
+++ product-of-digits.js
@@ -0,0 +1,10 @@
+function productOfDigits(num) {
+  let str = num.toString();
+  let total = 1;
+  for (let i = 0; i < str.length; i++) {
+    total *= Number(str[i]);
+  }
+  return total;
+}
+console.log(productOfDigits(123));
+console.log(productOfDigits(4040));
